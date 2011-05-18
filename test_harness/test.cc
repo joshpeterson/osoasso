030540689076271d7eaf49923174734d36debfc4
Joshua Peterson <petersonjm1@gmail.com>
2011-05-18 07:00:17 -0400

    Change the test harness to allow multiple test files in one executable.

---
+++ test_harness/test.cc
@@ -0,0 +1,10 @@
+#include "test.h"
+
+int main(int argc char** argv)
+{
+    bool all_tests_passed__ = true;
+
+    // Add RUN_TEST_FIXTURE statements here to run tests.
+
+    return all_tests_passed__ ? 0 : 1;
+}
