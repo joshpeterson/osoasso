8c9904682f702bd014b0abe7a69098c2897f1bec
Joshua Peterson <petersonjm1@gmail.com>
2011-05-20 07:32:43 -0400

    Start copying the public domain SHA1 implementation.

--- include/sha1.h
+++ include/sha1.h
@@ -1,6 +1,32 @@
 #ifndef __SHA1_H
 #define __SHA1_H
 
+#include <vector>
+#include <string>
+
+#define SHA1_DIGEST_SIZE 20
+
+#define rol(value, bits) (((value) << (bits)) | ((value) >> (32 - (bits))))
+
+/* blk0() and blk() perform the initial expand. */
+/* I got the idea of expanding during the round function from SSLeay */
+/* FIXME: can we do this in an endian-proof way? */
+#ifdef WORDS_BIGENDIAN
+#define blk0(i) block->l[i]
+#else
+#define blk0(i) (block->l[i] = (rol(block->l[i],24)&0xFF00FF00) \
+    |(rol(block->l[i],8)&0x00FF00FF))
+#endif
+#define blk(i) (block->l[i&15] = rol(block->l[(i+13)&15]^block->l[(i+8)&15] \
+    ^block->l[(i+2)&15]^block->l[i&15],1))
+
+/* (R0+R1), R2, R3, R4 are the different operations used in SHA1 */
+#define R0(v,w,x,y,z,i) z+=((w&(x^y))^y)+blk0(i)+0x5A827999+rol(v,5);w=rol(w,30);
+#define R1(v,w,x,y,z,i) z+=((w&(x^y))^y)+blk(i)+0x5A827999+rol(v,5);w=rol(w,30);
+#define R2(v,w,x,y,z,i) z+=(w^x^y)+blk(i)+0x6ED9EBA1+rol(v,5);w=rol(w,30);
+#define R3(v,w,x,y,z,i) z+=(((w|x)&y)|(w&x))+blk(i)+0x8F1BBCDC+rol(v,5);w=rol(w,30);
+#define R4(v,w,x,y,z,i) z+=(w^x^y)+blk(i)+0xCA62C1D6+rol(v,5);w=rol(w,30);
+
 namespace osoasso
 {
 
@@ -8,6 +34,23 @@ class sha1
 {
 public:
     sha1();
+    ~sha1();
+
+    std::string hash(const std::vector<unsigned char>& data);
+
+private:
+    typedef struct {
+        uint32_t state[5];
+        uint32_t count[2];
+        uint8_t  buffer[64];
+    } SHA1_CTX;
+
+    SHA1_CTX* context;
+
+    void SHA1_Update(const uint8_t* data, const size_t len);
+    void SHA1_Final(uint8_t digest[SHA1_DIGEST_SIZE]);
+    void SHA1_Transform(uint32_t state[5], const uint8_t buffer[64]);
+    void digest_to_hex(const uint8_t digest[SHA1_DIGEST_SIZE], char *output);
 };
 
 }
--- test/test_sha1.cc
+++ test/test_sha1.cc
@@ -1,3 +1,4 @@
+#include <vector>
 #include "../test_harness/test.h"
 #include "../include/sha1.h"
 
@@ -8,7 +9,16 @@ TEST_FIXTURE_START(TestSHA1)
 TEST_START(VerifyShortString)
 
     sha1 hasher;
-    ASSERT_EQUAL("a9993e36 4706816a ba3e2571 7850c26c 9cd0d89d", hasher.hash());
+    std::vector<unsigned char> data = { 'a', 'b', 'c' };
+    ASSERT_EQUAL("a9993e36 4706816a ba3e2571 7850c26c 9cd0d89d", hasher.hash(data));
+
+TEST_END
+
+TEST_START(VerifyLongString)
+
+    sha1 hasher;
+    std::vector<unsigned char> data = { 'a','b','c','d','b','c','d','e','c','d','e','f','d','e','f','g','e','f','g','h','f','g','h','i','g','h','i','j','h','i','j','k','i','j','k','l','j','k','l','m','k','l','m','n','l','m','n','o','m','n','o','p','n','o','p','q' };
+    ASSERT_EQUAL("84983e44 1c3bd26e baae4aa1 f95129e5 e54670f1", hasher.hash(data));
 
 TEST_END
 
