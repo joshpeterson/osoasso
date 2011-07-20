c73d9b685dfaf434f92cef1e92f0ec855cccb2e2
Joshua Peterson <petersonjm1@gmail.com>
2011-07-20 07:12:26 -0400

    Added the namepsace to the trim method.

--- include/trim.h
+++ include/trim.h
@@ -1,6 +1,9 @@
 #ifndef __TRIM_H
 #define __TRIM_H
 
+namespace osoasso
+{
+
 inline std::string trim(const std::string& value)
 {
     std::string::size_type first_non_whitespace = value.find_first_not_of(" \t\n\r");
@@ -12,6 +15,8 @@ inline std::string trim(const std::string& value)
     std::string::size_type last_non_whitespace = value.find_last_not_of(" \t\n\r");
 
     return value.substr(first_non_whitespace, last_non_whitespace - first_non_whitespace + 1);
+}
+
 }
 
 #endif // __TRIM_H
