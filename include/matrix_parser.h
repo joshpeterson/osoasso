97a0477508f885a642cf30f7ae1973fd12a9de52
Joshua Peterson <petersonjm1@gmail.com>
2011-07-19 21:27:19 -0400

    Extract out a parse row method.

--- include/matrix_parser.h
+++ include/matrix_parser.h
@@ -25,8 +25,8 @@ public:
         matrix_builder<ValueType> builder;
 
         bool first_row = true;
-        size_t this_row_start_bracket_position = value_to_parse_.find_first_of('[', 1);
-        while (this_row_start_bracket_position != std::string::npos)
+        size_t row_start_bracket_position = value_to_parse_.find_first_of('[', 1);
+        while (row_start_bracket_position != std::string::npos)
         {
             if (!first_row)
             {
@@ -37,22 +37,15 @@ public:
                 first_row = false;
             }
 
-            size_t this_row_end_bracket_position = value_to_parse_.find_first_of(']', this_row_start_bracket_position);
-            if (this_row_end_bracket_position != std::string::npos)
+            size_t row_end_bracket_position = value_to_parse_.find_first_of(']', row_start_bracket_position);
+            if (row_end_bracket_position != std::string::npos)
             {
-                std::istringstream stream(value_to_parse_.substr(this_row_start_bracket_position+1, this_row_end_bracket_position - (this_row_start_bracket_position+1)));
-                std::vector<std::string> tokens;
-                std::copy(std::istream_iterator<std::string>(stream), std::istream_iterator<std::string>(), std::back_inserter<std::vector<std::string>>(tokens));
-                for (auto i = tokens.begin(); i != tokens.end(); ++i)
-                {
-                    std::istringstream to_double(*i);
-                    double value;
-                    to_double >> value;
-                    builder.add(value);
-                }
+                std::string row_string_without_brackets = value_to_parse_.substr(row_start_bracket_position+1,
+                                                                                 row_end_bracket_position - (row_start_bracket_position+1));
+                parse_row(row_string_without_brackets, builder);
             }
 
-            this_row_start_bracket_position = value_to_parse_.find_first_of('[', this_row_end_bracket_position+1);
+            row_start_bracket_position = value_to_parse_.find_first_of('[', row_end_bracket_position+1);
         }
 
         return builder.done();
@@ -60,6 +53,23 @@ public:
 
 private:
     std::string value_to_parse_;
+
+    void parse_row(std::string row_string, matrix_builder<ValueType>& builder) const
+    {
+        std::istringstream row_stream(row_string);
+        std::vector<std::string> row_entries;
+        std::copy(std::istream_iterator<std::string>(row_stream),
+                  std::istream_iterator<std::string>(),
+                  std::back_inserter<std::vector<std::string>>(row_entries));
+
+        for (auto i = row_entries.begin(); i != row_entries.end(); ++i)
+        {
+            std::istringstream to_value(*i);
+            ValueType value;
+            to_value >> value;
+            builder.add(value);
+        }
+    }
 };
 
 }
