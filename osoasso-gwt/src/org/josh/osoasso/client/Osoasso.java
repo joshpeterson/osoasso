9fbf75a243cd95445f520ed114e07dd462eba0ff
Joshua Peterson <petersonjm1@gmail.com>
2011-11-14 19:43:35 -0500

    Make some intermediate changes to attempt to get the GUI working.

--- osoasso-gwt/src/org/josh/osoasso/client/Osoasso.java
+++ osoasso-gwt/src/org/josh/osoasso/client/Osoasso.java
@@ -2,6 +2,7 @@ package org.josh.osoasso.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyPressEvent;
@@ -9,6 +10,8 @@ import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
@@ -36,10 +39,13 @@ public class Osoasso extends Composite implements EntryPoint {
 
 	interface OsoassoUiBinder extends UiBinder<DockLayoutPanel, Osoasso> {}
 	
+	int type;
+	
 	@UiField TextBox inputField;
 	@UiField ScrollPanel scrollPanel;
 	@UiField VerticalPanel resultsPanel;
 	@UiField OsoassoStyle style;
+	@UiField Label moduleStatus;
 
 	public Osoasso()
 	{
@@ -54,7 +60,15 @@ public class Osoasso extends Composite implements EntryPoint {
 		Window.enableScrolling(false);
 	    Window.setMargin("0px");
 	    
+	    final NaclWidget naclWidget = new NaclWidget();
+	    naclWidget.setHTML("<embed name=\"nacl_module\" "
+	                       + "id=\"osoasso\" "
+	                       + "width=0 height=0 "
+	                       + "src=\"osoasso.nmf\" "
+	                       + "type=\"application/x-nacl\" />");
+	    
 	    RootLayoutPanel root = RootLayoutPanel.get();
+	    root.add(naclWidget);
 	    root.add(outer);
 	}
 	
@@ -63,11 +77,57 @@ public class Osoasso extends Composite implements EntryPoint {
 	{
 		if (e.getCharCode() == KeyCodes.KEY_ENTER)
 		{
-			HTML data = new HTML("> " + inputField.getText());
-			data.getElement().addClassName(style.individualOutputArea());
-		    resultsPanel.add(data);
+			//HTML data = new HTML("> " + inputField.getText());
+			//data.getElement().addClassName(style.individualOutputArea());
+		    //resultsPanel.add(data);
+			CallNaClInput(naclModule, inputField.getText());
+			
 		    scrollPanel.scrollToBottom();
 		}
 	}
-
+	
+	public void onNaclMessage(String data)
+	{
+		HTML html = new HTML("> Boo!");
+		html.getElement().addClassName(style.individualOutputArea());
+	    resultsPanel.add(html);
+	}
+	
+	 protected static native JavaScriptObject GetNaclModule() /*-{
+	    return $doc.getElementById("osoasso");
+	  }-*/;
+	 
+	 public native void RegisterNaclListener(JavaScriptObject naclModule) /*-{
+	 	//this.@org.josh.osoasso.client.Osoasso::onNaclMessage(Ljava/lang/String;)("foo");
+	 	//naclModule.addEventListener('message', function(message_event) {
+      //this.@org.josh.osoasso.client.Osoasso::onNaclMessage(Ljava/lang/String;)(message_event.data);
+    //}, false);
+    naclModule.addEventListener('message', function(message_event) {
+      alert(message_event.data);
+    }, false);
+	// naclModule.addEventListener("message", $wnd.handleMessage, false);
+	 }-*/;
+	 
+	 protected static native void CallNaClInput(
+		      JavaScriptObject naclModule, String input) /*-{
+		    if (naclModule != null) {
+		      naclModule.postMessage('input:' + input + ":me@it.com");
+		    } else {
+		      alert("Nacl module not loaded");
+		    }
+		  }-*/;
+	 
+	 private JavaScriptObject naclModule = null;
+	  public class NaclWidget extends HTML {
+	    @Override
+	    protected void onLoad(){
+	      naclModule = GetNaclModule();
+	      if (naclModule != null) {
+	    	  moduleStatus.setText("Module loaded");
+	    	  RegisterNaclListener(naclModule);
+	      } else {
+	    	  moduleStatus.setText("Module failed to load");
+	      }
+	    }
+	  }
 }
