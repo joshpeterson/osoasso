c8ecfad18a0afd15fc3dbfa1f2fd9a89cbf64dd4
Joshua Peterson <petersonjm1@gmail.com>
2011-12-07 07:31:49 -0500

    Add an object to parse the commit string coming from native code.

---
+++ osoasso-gwt/src/org/josh/osoasso/client/CommitData.java
@@ -0,0 +1,50 @@
+package org.josh.osoasso.client;
+
+public class CommitData
+{
+	enum commitStringIndexes
+	{
+		NAME,
+		ACTION, 
+		USER,
+		TIME,
+		OUTPUT,
+		MATRIX
+	}
+	private String[] commitStringParts;
+	
+	public CommitData(String commitString)
+	{
+		commitStringParts = commitString.split(":");
+	}
+	
+	public String getName()
+	{
+		return commitStringParts[commitStringIndexes.NAME.ordinal()];
+	}
+	
+	public String getAction()
+	{
+		return commitStringParts[commitStringIndexes.ACTION.ordinal()];
+	}
+
+	public String getUser()
+	{
+		return commitStringParts[commitStringIndexes.USER.ordinal()];
+	}
+
+	public String getTime()
+	{
+		return commitStringParts[commitStringIndexes.TIME.ordinal()];
+	}
+	
+	public String getOutput()
+	{
+		return commitStringParts[commitStringIndexes.OUTPUT.ordinal()];
+	}
+	
+	public String getMatrix()
+	{
+		return commitStringParts[commitStringIndexes.MATRIX.ordinal()];
+	}
+}
