dd85119b11666c3b67b740249fb27bee3b441f97
Joshua Peterson <petersonjm1@gmail.com>
2012-01-31 19:55:56 -0500

    Change the timer to use gettimeofday() instead of std::clock()
    The discussion here:
    
    https://groups.google.com/group/native-client-discuss/browse_thread/thread/925ec6b67e4339a3/03282624c84423bc?lnk=gst&q=clock#03282624c84423bc
    
    indicates that the std::clock() functionality was broken at one point.
    It seems that it should be fixed in Chrome 16 and pepper 15, but it is
    not.  I have followed the advice in the thread and switched to use
    the gettimeofday() method instead of std::clock().  I have also changed
    the unit test runner to use the timer class, so it will pick up the correct
    implementation.

--- include/timer.h
+++ include/timer.h
@@ -3,6 +3,12 @@
 
 #include <ctime>
 
+//#define USE_CLOCK_IMPLEMENTATION
+
+#ifndef USE_CLOCK_IMPLEMENTATION
+#include <sys/time.h>
+#endif
+
 namespace osoasso
 {
 
@@ -11,16 +17,35 @@ class timer
 public:
     timer()
     {
+#ifdef USE_CLOCK_IMPLEMENTATION
         start_time_ = std::clock();
+#else
+        timeval time;
+        gettimeofday(&time, NULL);
+        start_time_value_ = time.tv_sec+(time.tv_usec/1000000.0);
+#endif
+        
     }
 
     double elapsed() const
     {
+#ifdef USE_CLOCK_IMPLEMENTATION
         return double(std::clock() - start_time_) / CLOCKS_PER_SEC;
+#else
+        timeval time;
+        gettimeofday(&time, NULL);
+        double end_time_value = time.tv_sec+(time.tv_usec/1000000.0);
+
+        return end_time_value - start_time_value_;
+#endif
     }
 
 private:
+#ifdef USE_CLOCK_IMPLEMENTATION
     std::clock_t start_time_;
+#else
+    double start_time_value_;
+#endif
 };
 
 }
