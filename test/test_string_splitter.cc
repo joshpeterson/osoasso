7bcf19e7e96728d195c1626dc2372e1953af242f
Joshua Peterson <petersonjm1@gmail.com>
2012-02-14 20:43:50 -0500

    Add the string splitter class.

---
+++ test/test_string_splitter.cc
@@ -0,0 +1,43 @@
+#include "../test_harness/test.h"
+#include "../include/string_splitter.h"
+
+using namespace osoasso;
+
+Define(StringSplitter)
+{
+    It("Returns the number of parts after splitting")
+    {
+        string_splitter splitter("The quick brown fox.", 4);
+        AssertEqual(5, splitter.number_of_parts());
+    } Done
+
+    It("Returns the number of parts after splitting when the parts are not the same size")
+    {
+        string_splitter splitter("The quick brown fox.", 3);
+        AssertEqual(7, splitter.number_of_parts());
+    } Done
+
+    It("Returns the correct first part of the string")
+    {
+        string_splitter splitter("The quick brown fox.", 4);
+        AssertEqual(std::string("The "), splitter.part(0));
+    } Done
+
+    It("Returns the correct second part of the string")
+    {
+        string_splitter splitter("The quick brown fox.", 4);
+        AssertEqual(std::string("quic"), splitter.part(1));
+    } Done
+
+    It("Returns the correct last part of the string")
+    {
+        string_splitter splitter("The quick brown fox.", 4);
+        AssertEqual(std::string("fox."), splitter.part(4));
+    } Done
+
+    It("Returns the correct last part of the string when the parts are not the same size")
+    {
+        string_splitter splitter("The quick brown fox.", 3);
+        AssertEqual(std::string("x."), splitter.part(6));
+    } Done
+}
