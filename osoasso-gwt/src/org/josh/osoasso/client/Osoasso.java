2ab388ae3e18270074db02a91d35b1b1b82c0e16
Joshua Peterson <petersonjm1@gmail.com>
2012-03-08 00:27:29 -0500

    Add an inital help message.
    We need to schedule a timer to wait for the NaCl module to load, then call
    across to it to print the help message.  If we don't get a response after a
    given length of time, assume that it did not load, and display an error message.

--- osoasso-gwt/src/org/josh/osoasso/client/Osoasso.java
+++ osoasso-gwt/src/org/josh/osoasso/client/Osoasso.java
@@ -11,6 +11,7 @@ import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DockLayoutPanel;
@@ -53,6 +54,10 @@ public class Osoasso extends Composite implements EntryPoint {
 	private JavaScriptObject naclModule = null;
 	
 	private StringConcatenator concatenator = new StringConcatenator();
+	
+	private boolean initalHelpMessageDisplayed = false;
+	
+	private Timer naclModuleLoadErrorTimer;
 	
 	final private String UsernameCookieName = "OsoassoUsernameCookie";
 	
@@ -100,6 +105,29 @@ public class Osoasso extends Composite implements EntryPoint {
 	                       + "src=\"osoasso.nmf\" "
 	                       + "type=\"application/x-nacl\" />");
 	    
+	    // Schedule a timer to display a help message when the NaCl executable loads.
+	    Timer displayHelpTimer = new Timer()
+	    {
+	        public void run()
+	        {
+	        	AddHTMLToResultsPanel("Attempting to communicate with the Osoasso NaCl executable...");
+	        	CallOsoassoNaclModuleInputMethod(naclModule, "help", usernameField.getText());
+	        	inputField.setReadOnly(true);
+	        }
+	     };
+	     displayHelpTimer.schedule(1000);
+	     
+	     // If the executable doesn't load in some time, print an error message.
+     	 naclModuleLoadErrorTimer = new Timer()
+     	 {
+     		 public void run()
+     		 {
+     			 AddHTMLToResultsPanel("It seems the Osoasso NaCl executable did not load.");
+     			 AddHTMLToResultsPanel("Make sure you are using the Google Chrome browser with Native Client enabled in about:flags.");
+     		 }
+     	 };
+     	 naclModuleLoadErrorTimer.schedule(5000);
+	    
 	    RootLayoutPanel root = RootLayoutPanel.get();
 	    root.add(naclWidget);
 	    root.add(outer);
@@ -145,7 +173,16 @@ public class Osoasso extends Composite implements EntryPoint {
 			else if (message.startsWith("text"))
 			{
 				TextFormatter formatter = new TextFormatter(message);
-				this.AddHTMLToResultsPanel(formatter.formatAction());
+				if (this.initalHelpMessageDisplayed)
+				{
+					this.AddHTMLToResultsPanel(formatter.formatAction());
+				}
+				else
+				{
+					naclModuleLoadErrorTimer.cancel();
+					this.initalHelpMessageDisplayed = true;
+				}
+				
 				for (String line: formatter.formatLines())
 				{
 					this.AddHTMLToResultsPanel(line);
