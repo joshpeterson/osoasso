d1adfef1c696a53a6ef8a8529350709b3d5ac49d
Joshua Peterson <petersonjm1@gmail.com>
2012-03-15 07:09:51 -0400

    Add stress test for the row and column iterators
    It seems that these tests may not be too useful, since both row
    and column iteration require near zero time on matrices up to 5000
    rows and columns.  I'm using 1024 for the matrix size to limit the
    time spent initializing the matrices.  Since I have these tests, I'll
    commit them.

---
+++ test/stress_test_column_iterator.cc
@@ -0,0 +1,34 @@
+#include <memory>
+#include <random>
+#include <iostream>
+#include "../test_harness/test.h"
+#include "../include/timer.h"
+#include "../include/matrix.h"
+
+using namespace osoasso;
+
+Define(StressTestColumnIterator)
+{
+    It("Iterates a 1024x1024 matrix by column")
+    {
+        srand(time(NULL));
+
+        auto big = std::make_shared<matrix<double>>(1024, 1024);
+
+        for (size_t row = 1; row <= 1024;  ++row)
+        {
+            for (size_t column = 1; column <= 1024; ++column)
+            {
+                (*big)(row, column) = rand();
+            }
+        }
+
+        timer column_iteration_timer;
+
+        for (auto it = big->column_begin(); it != big->column_end(); ++it)
+        {
+        }
+
+        std::cout << " Time to iterate columns: " << column_iteration_timer.elapsed() << "s" << std::endl;
+    } Done
+}
