9cfab3f34a67c38d4c8a0eef2d99a0b905e10b55
Joshua Peterson <petersonjm1@gmail.com>
2012-09-06 07:24:51 -0400

    Implement the method to find the greatest off diagonal entry in each row.

--- include/max_off_diagonal_indexes.h
+++ include/max_off_diagonal_indexes.h
@@ -3,15 +3,26 @@
 
 #include <vector>
 #include <memory>
+#include <cmath>
 #include "matrix.h"
 
 namespace osoasso
 {
 
 template <typename ValueType>
-std::vector<ValueType> max_off_diagonal_indexes(std::shared_ptr<matrix<ValueType>> input)
+std::vector<int> max_off_diagonal_indexes(const matrix<ValueType>& input)
 {
-    std::vector<ValueType> indexes;
+    std::vector<int> indexes(input.rows());
+
+    for (size_t i = 1; i <= input.rows(); ++i)
+    {
+        ValueType maximum_value_in_row = ValueType();
+        for (size_t j = 1; j <= input.columns(); ++j)
+        {
+            if (i != j && std::abs(input(i,j)) > std::abs(maximum_value_in_row))
+                indexes[i-1] = j;
+        }
+    }
 
     return indexes;
 }
--- test/test_max_off_diagonal_indexes.cc
+++ test/test_max_off_diagonal_indexes.cc
@@ -10,8 +10,22 @@ Define(MaxOffDiagonalIndexes)
     It ("Finds a vector of the non-diagonal indexes which have the greatest value in each row")
     {
         auto input = std::shared_ptr<matrix<double>>(new matrix<double>({{3.14, 1.0, 3.10},
-                                                                          {1.0, 1.0, 9.41},
-                                                                          {3.10, 9.41, 3.14}}));
-        /*auto result = */max_off_diagonal_indexes(input);
+                                                                         {1.0, 1.0, 9.41},
+                                                                         {3.10, 9.41, 3.14}}));
+        std::vector<int> expected = {3, 3, 2};
+        auto result = max_off_diagonal_indexes(*input);
+        
+        AssertElementsEqual(expected, result);
+    } Done
+
+    It ("Finds a vector of the non-diagonal indexes which have the greatest absolute value in each row")
+    {
+        auto input = std::shared_ptr<matrix<double>>(new matrix<double>({{3.14, 1.0, 3.10},
+                                                                         {1.0, 1.0, 9.41},
+                                                                         {3.10, -9.41, 3.14}}));
+        std::vector<int> expected = {3, 3, 2};
+        auto result = max_off_diagonal_indexes(*input);
+        
+        AssertElementsEqual(expected, result);
     } Done
 }
