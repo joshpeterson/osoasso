ffc1d7163dd7b4128378e9eb78e297115b76f45f
Joshua Peterson <petersonjm1@gmail.com>
2012-09-30 08:29:47 -0400

    Improve the way that the test can validate eigenvalues.

--- test/test_jacobi_eigen_solver.cc
+++ test/test_jacobi_eigen_solver.cc
@@ -5,87 +5,67 @@ using namespace osoasso;
 
 const double threshold = 0.0001;
 
-void find_eigenvalue(std::shared_ptr<matrix<double>> input, double eigenvalue)
+void validate_eigenvalues(const std::vector<double>& expected_eigenvalues, std::shared_ptr<const matrix<double>> actual_eigenvalues)
 {
-    jacobi_eigen_solver jacobi_command;
-
-    auto result = jacobi_command.call(input, 1);
-
-    bool eigenvalue_found = false;
-    for (auto it = result->begin(); it != result->end() && !eigenvalue_found; ++it)
+    for (auto expected_eigenvalue = expected_eigenvalues.begin(); expected_eigenvalue != expected_eigenvalues.end(); ++expected_eigenvalue)
     {
-        if (AreEqual(eigenvalue, *it, threshold))
-            eigenvalue_found = true;
-    }
-
-    if (!eigenvalue_found)
-    {
-        std::stringstream message;
-        message << "The eigenvalue " << eigenvalue << " was not found in the result: ";
-        for (auto it = result->begin(); it != result->end(); ++it)
+        bool eigenvalue_found = false;
+        for (auto actual_eigenvalue = actual_eigenvalues->begin(); actual_eigenvalue != actual_eigenvalues->end() && !eigenvalue_found; ++actual_eigenvalue)
         {
-            message << *it << " ";
+            if (AreEqual(*expected_eigenvalue, *actual_eigenvalue, threshold))
+                eigenvalue_found = true;
         }
-        message << std::endl;
 
-        AssertFail(message.str());
+        if (!eigenvalue_found)
+        {
+            std::stringstream message;
+            message << "The eigenvalue " << *expected_eigenvalue << " was not found in the result: ";
+            for (auto it = actual_eigenvalues->begin(); it != actual_eigenvalues->end(); ++it)
+            {
+                message << *it << " ";
+            }
+            message << std::endl;
+
+            AssertFail(message.str());
+        }
     }
 }
 
 Define(JacobiEigenSolver)
 {
-    It ("Finds the first eigenvalue of a 3x3 symmetric matrix")
-    {
-        auto input = std::shared_ptr<matrix<double>>(new matrix<double>({{1, 0, 2}, {0, 2, 1}, {2, 1, 1}}));
-        find_eigenvalue(input, 1.7728);
-    } Done
-
-    It ("Finds the second eigenvalue of a 3x3 symmetric matrix")
-    {
-        auto input = std::shared_ptr<matrix<double>>(new matrix<double>({{1, 0, 2}, {0, 2, 1}, {2, 1, 1}}));
-        find_eigenvalue(input, -1.16424);
-    } Done
-
-    It ("Finds the third eigenvalue of a 3x3 symmetric matrix")
-    {
-        auto input = std::shared_ptr<matrix<double>>(new matrix<double>({{1, 0, 2}, {0, 2, 1}, {2, 1, 1}}));
-        find_eigenvalue(input, 3.39138);
-    } Done
-
-    It ("Finds the first unique eigenvalue of another 3x3 symmetric matrix")
-    {
-        // This matrix has eigenvalues of -1, -1, and 8
-        auto input = std::shared_ptr<matrix<double>>(new matrix<double>({{3, 2, 4}, {2, 0, 2}, {4, 2, 3}}));
-        find_eigenvalue(input, -1);
-    } Done
-
-    It ("Finds the second unique eigenvalue of another 3x3 symmetric matrix")
+    It ("Finds the eigenvalues of another 3x3 symmetric matrix")
     {
         auto input = std::shared_ptr<matrix<double>>(new matrix<double>({{3, 2, 4}, {2, 0, 2}, {4, 2, 3}}));
-        find_eigenvalue(input, 8);
+        std::vector<double> expected_eigenvalues = { -1, -1, 8 };
+
+        jacobi_eigen_solver jacobi_command;
+
+        auto result = jacobi_command.call(input, 1);
+
+        validate_eigenvalues(expected_eigenvalues, result);
     } Done
 
-    It ("Finds the first eigenvalue of a 4x4 symmetric matrix")
+    It ("Finds the eigenvalues of a 4x4 symmetric matrix")
     {
-        auto input = std::shared_ptr<matrix<double>>(new matrix<double>({{-5, -4, 5, -0}, {-4, 2, 0, 9}, {5,0,9,8}, {0, 9, 8, 2}}));
-        find_eigenvalue(input, -9.30949);
-    } Done
+        auto input = std::shared_ptr<matrix<double>>(new matrix<double>({{1, 0, 2}, {0, 2, 1}, {2, 1, 1}}));
+        std::vector<double> expected_eigenvalues = { 1.7728, -1.16424, 3.39138 };
 
-    It ("Finds the second eigenvalue of a 4x4 symmetric matrix")
-    {
-        auto input = std::shared_ptr<matrix<double>>(new matrix<double>({{-5, -4, 5, -0}, {-4, 2, 0, 9}, {5,0,9,8}, {0, 9, 8, 2}}));
-        find_eigenvalue(input, -7.53826);
-    } Done
+        jacobi_eigen_solver jacobi_command;
 
-    It ("Finds the third eigenvalue of a 4x4 symmetric matrix")
-    {
-        auto input = std::shared_ptr<matrix<double>>(new matrix<double>({{-5, -4, 5, -0}, {-4, 2, 0, 9}, {5,0,9,8}, {0, 9, 8, 2}}));
-        find_eigenvalue(input, 8.31277);
+        auto result = jacobi_command.call(input, 1);
+
+        validate_eigenvalues(expected_eigenvalues, result);
     } Done
 
     It ("Finds the fourth eigenvalue of a 4x4 symmetric matrix")
     {
-        auto input = std::shared_ptr<matrix<double>>(new matrix<double>({{-5, -4, 5, -0}, {-4, 2, 0, 9}, {5,0,9,8}, {0, 9, 8, 2}}));
-        find_eigenvalue(input, 16.53498);
+        auto input = std::shared_ptr<matrix<double>>(new matrix<double>({{-5, -4, 5, -0}, {-4, 2, 0, 9}, {5, 0, 9, 8}, {0, 9, 8, 2}}));
+        std::vector<double> expected_eigenvalues = { -9.30949, -7.53826, 8.31277, 16.53498 };
+
+        jacobi_eigen_solver jacobi_command;
+
+        auto result = jacobi_command.call(input, 1);
+
+        validate_eigenvalues(expected_eigenvalues, result);
     } Done
 }
