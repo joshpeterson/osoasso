d6c74325e11b313f1b046093d057424786d26cef
Joshua Peterson <petersonjm1@gmail.com>
2014-04-14 07:05:55 -0400

    Add some simple logging to stdout in C++.

---
+++ include/log.h
@@ -0,0 +1,25 @@
+#ifndef __LOG_H
+#define __LOG_H
+
+#include <string>
+#include <iostream>
+
+#define ENABLE_LOGGING
+
+#ifdef ENABLE_LOGGING
+#define LOG(message) osoasso::log(message)
+#else
+#define LOG(message)
+#endif
+
+namespace osoasso
+{
+
+void log(const std::string& message)
+{
+    std::cout << "osoasso: " << message << std::endl;
+}
+
+}
+
+#endif // __LOG_H
